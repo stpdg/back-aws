23b43f7d19c649cd2cae6efac9ca167fc6cb157f
Hog <6062207@gmail.com>
2023-11-26 17:17:04 +0100

    init:start task4

--- lib/back-aws-stack.ts
+++ lib/back-aws-stack.ts
@@ -3,10 +3,34 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as iam from "aws-cdk-lib/aws-iam";
+
 
 export class BackAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const dynamoRole = new iam.Role(this, "dynamoRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      inlinePolicies: {
+        dynamoDBAccessPolicy: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: [
+                "dynamodb:Scan",
+                "dynamodb:GetItem",
+                "dynamodb:PutItem",
+              ],
+              resources: [
+              ],
+            }),
+          ],
+        }),
+      },
+    });
+
+
 
     const getProductsList = new NodejsFunction(this, 'GetProductsList', {
       functionName: 'getProductsList',
